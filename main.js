3f15e0550d1f65feacc1b41c7150bd954f8f2276
Kristian <kburiasco@gmail.com>
2022-11-05 23:36:37 +0100

    dark mode working and new input type email desing

--- main.js
+++ main.js
@@ -11,3 +11,15 @@ navToggle.addEventListener("click", () => {
   disablescroll.toggleAttribute("disableoverflow");
 });
 
+// function myFunction() {
+//   var element = document.body;
+//   element.classList.toggle("dark-mode");
+// }
+
+function darking_mode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  var element2 = document.querySelector(".icon-hamburger");
+  element2.classList.toggle("svg-dark");
+};
+document.getElementById("dark-mode-button").onclick = darking_mode;
