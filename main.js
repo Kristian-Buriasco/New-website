60c3e7df85d406dddf6f63c9e5b4f9e17f94301a
Kristian <kburiasco@gmail.com>
2022-11-05 18:29:27 +0100

    menu, footer, and css stuff but nothing else

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const primaryHeader = document.querySelector('.primary-header');
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-nav");
+const disablescroll = document.querySelector(".body");
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")
     ? navToggle.removeAttribute("aria-expanded", true)
     : navToggle.setAttribute("aria-expanded", false);
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
+  disablescroll.toggleAttribute("disableoverflow");
 });
+
